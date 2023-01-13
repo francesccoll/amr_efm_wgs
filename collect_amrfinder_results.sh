06c9d001ebd5759a2fe91e4b8697b66869276de4
francesccoll <51442218+francesccoll@users.noreply.github.com>
2023-01-13 15:43:32 +0000

    Add files via upload

---
+++ collect_amrfinder_results.sh
@@ -0,0 +1,4 @@
+
+
+
+python3 collect_amrfinder_results.py -a amrfinder_efm_antibiotics.txt -i efm_dst_dataset_v3.all.sample_ids.txt -d ./amrfinder_results/ -s .amrfinder_output.txt -p efm_dst_dataset_v3.amrfinder_phenotypes.efm.csv -g efm_dst_dataset_v3.amrfinder_genotypes.efm.csv
